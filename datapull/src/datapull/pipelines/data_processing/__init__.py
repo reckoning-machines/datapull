a82963c143a5536f19f664d4e216f97abe1a1a32
Jed Gore <jedgore@Jeds-MacBook-Pro.local>
2024-11-09 13:10:24 -0500

    change project name

---
+++ datapull/src/datapull/pipelines/data_processing/__init__.py
@@ -0,0 +1,3 @@
+"""Complete Data Processing pipeline for the datapull tutorial"""
+
+from .pipeline import create_pipeline  # NOQA
---
+++ datapull/src/datapull/pipelines/data_science/__init__.py
@@ -0,0 +1,3 @@
+"""Complete Data Science pipeline for the datapull tutorial"""
+
+from .pipeline import create_pipeline  # NOQA
